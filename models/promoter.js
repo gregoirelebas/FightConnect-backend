b788ded243790c85a01f930b9c5631df95c81231
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-10 17:00:07 +0100

    Removed duplicated field.

--- models/promoter.js
+++ models/promoter.js
@@ -1,22 +1,17 @@
 const mongoose = require('mongoose');
 
 const organizationSchema = mongoose.Schema({
-    name: String,
-    date: Date,
+  name: String,
+  date: Date,
 });
 
 const promoterSchema = mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref:'users'},
-    sport: String,
-    organization: String,
-    siretNumber: String,
-    organizations: [organizationSchema],
-    
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+  sport: String,
+  siretNumber: String,
+  organizations: [organizationSchema],
 });
-
 
 const Promoter = mongoose.model('promoters', promoterSchema);
 
 module.exports = Promoter;
-
-
