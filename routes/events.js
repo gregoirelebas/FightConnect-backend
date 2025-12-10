b081d8294740da5909ff41038a766b734b6fe1d0
kalifa8869 <kalifa.seydi@live.fr>
2025-12-10 18:03:22 +0100

    route event

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/events');
+const User = require('../models/users');
 
 
 const { checkBody } = require('../modules/checkBody');
@@ -8,16 +9,16 @@ const { checkBody } = require('../modules/checkBody');
 
 router.post('/create', async (req, res) => {
     if (!checkBody(req.body,
-        ['level', 'sports', 'clubName', 'date', 'experience', 'weight', 'name','description',
+        ['level', 'sports', 'clubName', 'date', 'experience', 'weight', 'minWeight', 'maxWeight', 'name', 'description',
             'PromoterId'])) {
         res.json({ result: false, error: 'Missing or empty fields' });
         return;
     }
 
-    const { level, sports, clubName, date, experience, weight, name, description,
-            PromoterId, } = req.body;
+    const { level, sports, clubName, date, experience, weight, minWeight, maxWeight, name, description,
+        PromoterId, } = req.body;
 
-  
+
 
     const newEvent = new Event({
         level: level,
@@ -26,6 +27,8 @@ router.post('/create', async (req, res) => {
         date: new Date(date),
         experience: experience,
         weight: weight,
+        minWeight: minWeight,
+        maxWeight: maxWeight,
         name: name,
         description: description,
         PromoterId: PromoterId,
@@ -34,9 +37,71 @@ router.post('/create', async (req, res) => {
 
     await newEvent.save();
 
-   res.send("ok");
+    res.send("ok");
 
 });
+
+router.get("/search", async (req, res) => {
+
+
+  try {
+    const data = await Event.find(req.query);
+
+    if (data.length > 0) {
+      res.json({ result: true, event: data });
+    } else {
+      res.json({ result: false, error: "No event found" });
+    }
+  } catch (error) {
+    res.status(500).json({ result: false, error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ result: false, error: "Missing id parameter" });
+    }
+
+    const event = await Event.findById(id);
+
+    if (!event) {
+      return res.status(404).json({ result: false, error: "Event not found" });
+    }
+
+    res.json({ result: true, event });
+  } catch (error) {
+    res.status(500).json({ result: false, error: error.message });
+  }
+});
+
+
+
+router.get("/user/:id", async (req, res) => {
+  try {
+    console.log(req.params)
+    const  PromoterId  = req.params.id;
+
+    if (!PromoterId) {
+      return res.status(400).json({ result: false, error: "Missing id parameter" });
+    }
+
+    const event = await Event.find({PromoterId});
+
+    if (!event) {
+      return res.status(404).json({ result: false, error: "User not found" });
+    }
+
+    res.json({ result: true, event });
+  } catch (error) {
+    res.status(500).json({ result: false, error: error.message });
+  }
+});
+
+
+
 
 
 module.exports = router;
